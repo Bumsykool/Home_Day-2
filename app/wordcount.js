272d700aecefff9cc7e444c8f1d2356d73d93acb
Olubunmi <Olubunmi>
2016-11-29 15:50:51 +0100

    update

--- app/wordcount.js
+++ app/wordcount.js
@@ -2,8 +2,19 @@ module.exports = {
 
 words: function (str){
 
-		return str.split("").length;
 
+	    var words = str.split(" ");
+        var count = {};
+        for (var i = 0, len = words.length; i < len; i++) {
+            if (count.hasOwnProperty(words[i])) {
+                count[words[i]] = parseInt(count[words[i]], 10) + 1;
+            }
+            else {
+                count[words[i]] = 1;
+            }
+        }
+        return count;
+    }
 
 
 
@@ -22,4 +33,3 @@ words: function (str){
 
 
 
-}
